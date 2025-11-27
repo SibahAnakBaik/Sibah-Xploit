c2e75ad45b1ed1528fe7d5f9a7b71bf538c6912b
SibahAnakBaik <workfire961@gmail.com>
2025-11-27 20:21:26 +0700

    Update AAPROJECT_HUB.lua

--- AAPROJECT_HUB.lua
+++ AAPROJECT_HUB.lua
@@ -382,7 +382,7 @@ local Window = Libary:MakeWindow({
     Flags = "RegzyyHub_Broookhaven"
 })
 Window:AddMinimizeButton({
-    Button = { Image = "rbxassetid://91651883440825", BackgroundTransparency = 0 },
+    Button = { Image = "87803715505272", BackgroundTransparency = 0 },
     Corner = { CornerRadius = UDim.new(35, 1) },
 })
 
@@ -399,7 +399,7 @@ InfoTab:AddSection({"Créditos do Hub"})
 InfoTab:AddDiscordInvite({
     Name = "RegzyyHub",
     Description = "https://discord.gg/94jaeGg6",
-    Logo = "rbxassetid://91651883440825",
+    Logo = "91651883440825",
     Invite = "https://discord.gg/94jaeGg6",
 })
 
