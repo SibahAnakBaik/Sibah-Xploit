355d3c75a77b53469c74744601d15c0bfe6361df
SibahAnakBaik <workfire961@gmail.com>
2025-11-27 22:13:05 +0700

    Update AAPROJECT_HUB.lua

--- AAPROJECT_HUB.lua
+++ AAPROJECT_HUB.lua
@@ -382,7 +382,7 @@ local Window = Libary:MakeWindow({
     Flags = "RegzyyHub_Broookhaven"
 })
 Window:AddMinimizeButton({
-    Button = { Image = "87803715505272", BackgroundTransparency = 0 },
+    Button = { Image = "rbxassetid://87803715505272", BackgroundTransparency = 0 },
     Corner = { CornerRadius = UDim.new(35, 1) },
 })
 
